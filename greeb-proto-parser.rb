f128eb3664ba6694250507d60cb6f9e9dcd6a202
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-02-02 15:49:04 +0500

    greeb-proto-parser: punctuation parser

--- greeb-proto-parser.rb
+++ greeb-proto-parser.rb
@@ -2,10 +2,12 @@
 # encoding: utf-8
 
 origin = <<-END
- - Сынок, чего это от тебя зигами пахнет,
+ - Сынок, чего это     от тебя   зигами пахнет,
 опять на Манежную площадь ходил?
 
- - Нет мама, я в метро ехал, там назиговано было!
+ - Нет мама, я в метро ехал, там  назиговано было!
+
+  
 
 Четырнадцать, восемьдесять восемь: 14/88.
 Вот так блять.
@@ -16,7 +18,7 @@ RU_LEX = /^[А-Яа-я]+$/u
 EN_LEX = /^[A-Za-z]+$/u
 EOL = /^\n+$/u
 SEP = /^[*=_ ]$/u
-PNC = /^(\.|\!|\?|\[|\]|\(|\)|\-|:|;)$/u
+PUN = /^(\.|\!|\?|\[|\]|\(|\)|\-|:|;)$/u
 DIG = /^[0-9]+$/u
 DIL = /^[А-Яа-яA-Za-z0-9]+$/u
 EMPTY = ''
@@ -29,8 +31,9 @@ def parse(origin)
 
   token = ''
 
+  puts '# Finite-State Automata'
   origin.each_char do |c|
-    p [ c, token ]
+    puts "[#{token.inspect}] ← #{c.inspect}"
     case c
       when EOL then begin
         case token
@@ -39,6 +42,7 @@ def parse(origin)
             token = ''
             text << [ [ ] ]
             paragraph_id = text.size - 1
+#            paragraph_id += 1
             sentence_id = 0
           end
           else
@@ -50,30 +54,57 @@ def parse(origin)
         unless token.empty?
           text[paragraph_id][sentence_id] << token
         end
+        while text[paragraph_id][sentence_id].last == c
+          text[paragraph_id][sentence_id].pop
+        end
         text[paragraph_id][sentence_id] << c
         token = ''
       end
+      when PUN then begin
+        case token
+          when EMPTY
+          else
+            text[paragraph_id][sentence_id] << token
+            text[paragraph_id][sentence_id] << c
+            token = ''
+            text[paragraph_id] << []
+            sentence_id = text[paragraph_id].size - 1
+#            sentence_id += 1
+        end
+      end
       when RU_LEX then begin
         case token
-          when EOL then token = c
+          when EOL then begin
+            text[paragraph_id][sentence_id] << ' '
+            token = c
+          end
           else token << c
         end
       end
       when EN_LEX then begin
         case token
-          when EOL then token = c
+          when EOL then begin
+            text[paragraph_id][sentence_id] << ' '
+            token = c
+          end
           else token << c
         end
       end
       when DIG then begin
         case token
-          when EOL then token = c
+          when EOL then begin
+            text[paragraph_id][sentence_id] << ' '
+            token = c
+          end
           else token << c
         end
       end
       when DIL then begin
         case token
-          when EOL then token = c
+          when EOL then begin
+            text[paragraph_id][sentence_id] << token
+            token = c
+          end
           else token << c
         end
       end
@@ -85,4 +116,15 @@ def parse(origin)
   text
 end
 
-p parse(origin)
+text = parse(origin)
+
+puts '# Parsing Result'
+p text
+
+puts '# Text Interpretation'
+pure = text.map do |paragraph|
+  paragraph.map do |sentence|
+    sentence.join
+  end.join(' ')
+end.join("\n\n")
+puts pure
