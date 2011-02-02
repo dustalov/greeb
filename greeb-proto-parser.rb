010ab62f891506b51691435ba68b383bf9c8744e
Dmitry A. Ustalov <dmitry@eveel.ru>
2011-02-02 14:46:24 +0500

    greeb-proto-parser: proper paragraph parser

--- greeb-proto-parser.rb
+++ greeb-proto-parser.rb
@@ -14,12 +14,12 @@ origin.chomp!
 
 RU_LEX = /^[А-Яа-я]+$/u
 EN_LEX = /^[A-Za-z]+$/u
-PAR = /^(\n)+$/u
+EOL = /^\n+$/u
 SEP = /^[*=_ ]$/u
 PNC = /^(\.|\!|\?|\[|\]|\(|\)|\-|:|;)$/u
 DIG = /^[0-9]+$/u
 DIL = /^[А-Яа-яA-Za-z0-9]+$/u
-EMPTY = /^$/u
+EMPTY = ''
 
 def parse(origin)
   text = [ [ [ ] ] ]
@@ -30,17 +30,20 @@ def parse(origin)
   token = ''
 
   origin.each_char do |c|
+    p [ c, token ]
     case c
-      when PAR then begin
+      when EOL then begin
         case token
           when EMPTY then token << c
-          when PAR then token << c
+          when EOL then begin
+            token = ''
+            text << [ [ ] ]
+            paragraph_id = text.size - 1
+            sentence_id = 0
+          end
           else
             text[paragraph_id][sentence_id] << token
             token = c
-            text << [ [ ] ]
-            paragraph_id += 1
-            sentence_id = 0
         end
       end
       when SEP then begin
@@ -52,25 +55,25 @@ def parse(origin)
       end
       when RU_LEX then begin
         case token
-          when PAR then token = c
+          when EOL then token = c
           else token << c
         end
       end
       when EN_LEX then begin
         case token
-          when PAR then token = c
+          when EOL then token = c
           else token << c
         end
       end
       when DIG then begin
         case token
-          when PAR then token = c
+          when EOL then token = c
           else token << c
         end
       end
       when DIL then begin
         case token
-          when PAR then token = c
+          when EOL then token = c
           else token << c
         end
       end
