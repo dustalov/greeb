4854609f990637db719f4e8fddc7c25bdff98df3
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-07-05 20:11:55 +0400

    Improve the code documentation [ci skip]

--- lib/greeb/tokenizer.rb
+++ lib/greeb/tokenizer.rb
@@ -34,11 +34,23 @@ class Greeb::Tokenizer
   #
   BREAKS = /\n+/u
 
+  # Greeb operates with tokens, tuples of `<from, to, kind>`, where
+  # `from` is a beginning of the token, `to` is an ending of the token,
+  # and `kind` is a type of the token.
+  #
+  # There are several token types: `:letter`, `:float`, `:integer`,
+  # `:separator`, `:punct` (for punctuation), `:spunct` (for in-sentence
+  # punctuation), and `:break`.
+  #
   Token = Struct.new(:from, :to, :kind)
 
   attr_reader :text, :tokens, :scanner
   protected :scanner
 
+  # Create a new instance of {Tokenizer}.
+  #
+  # @param text [String] text to be tokenized.
+  #
   def initialize(text)
     @text = text
     @tokens = Set.new
@@ -47,6 +59,11 @@ class Greeb::Tokenizer
   end
 
   protected
+    # Perform the tokenization process. This method modifies
+    # `@scanner` and `@tokens` instance variables.
+    #
+    # @return [nil] nothing unless exception is raised.
+    #
     def tokenize!
       @scanner = StringScanner.new(text)
       while !scanner.eos?
@@ -63,11 +80,30 @@ class Greeb::Tokenizer
       scanner.terminate
     end
 
+    # Try to parse one small piece of text that is covered by pattern
+    # of necessary kind.
+    #
+    # @param pattern [Regexp] a regular expression to extract the token.
+    # @param kind [Symbol] a symbol that represents the necessary token
+    # type.
+    #
+    # @return [Set<Token>] the modified set of extracted tokens.
+    #
     def parse! pattern, kind
       return false unless token = scanner.scan(pattern)
       @tokens << Token.new(scanner.pos - token.length, scanner.pos, kind)
     end
 
+    # Try to parse one small piece of text that is covered by pattern
+    # of necessary kind. This method performs grouping of the same
+    # characters.
+    #
+    # @param pattern [Regexp] a regular expression to extract the token.
+    # @param kind [Symbol] a symbol that represents the necessary token
+    # type.
+    #
+    # @return [Set<Token>] the modified set of extracted tokens.
+    #
     def split_parse! pattern, kind
       return false unless token = scanner.scan(pattern)
       position = scanner.pos - token.length
