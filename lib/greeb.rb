8c02b94fcde1ad23b1ffbedf9c553860e4869a52
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-07-08 01:56:06 +0600

    Replace Greeb::Tokenizer::Token with Greeb::Entity

--- lib/greeb.rb
+++ lib/greeb.rb
@@ -1,4 +1,16 @@
 # encoding: utf-8
 
 require 'greeb/version'
+
+# Greeb operates with tokens, tuples of `<from, to, kind>`, where
+# `from` is a beginning of the token, `to` is an ending of the token,
+# and `kind` is a type of the token.
+#
+# There are several token types: `:letter`, `:float`, `:integer`,
+# `:separ` for separators, `:punct` for punctuation characters,
+# `:spunct` for in-sentence punctuation characters, and
+# `:break` for line endings.
+#
+class Greeb::Entity < Struct.new(:from, :to, :type); end
+
 require 'greeb/tokenizer'
--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -39,51 +39,51 @@ module Greeb
     describe 'tokenization facilities' do
       it 'can handle words' do
         Tokenizer.new('hello').tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0, 5, :letter)])
+          Set.new([Entity.new(0, 5, :letter)])
         )
       end
 
       it 'can handle floats' do
         Tokenizer.new('14.88').tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0, 5, :float)])
+          Set.new([Entity.new(0, 5, :float)])
         )
       end
 
       it 'can handle integers' do
         Tokenizer.new('1337').tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0, 4, :integer)])
+          Set.new([Entity.new(0, 4, :integer)])
         )
       end
 
       it 'can handle words and integers' do
         Tokenizer.new('Hello, I am 18').tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0,  5,  :letter),
-                   Tokenizer::Token.new(5,  6,  :spunct),
-                   Tokenizer::Token.new(6,  7,  :separ),
-                   Tokenizer::Token.new(7,  8,  :letter),
-                   Tokenizer::Token.new(8,  9,  :separ),
-                   Tokenizer::Token.new(9,  11, :letter),
-                   Tokenizer::Token.new(11, 12, :separ),
-                   Tokenizer::Token.new(12, 14, :integer)])
+          Set.new([Entity.new(0,  5,  :letter),
+                   Entity.new(5,  6,  :spunct),
+                   Entity.new(6,  7,  :separ),
+                   Entity.new(7,  8,  :letter),
+                   Entity.new(8,  9,  :separ),
+                   Entity.new(9,  11, :letter),
+                   Entity.new(11, 12, :separ),
+                   Entity.new(12, 14, :integer)])
         )
       end
 
       it 'can handle multi-line paragraphs' do
         Tokenizer.new("Brateeshka..!\n\nPrines!").tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0,  10, :letter),
-                   Tokenizer::Token.new(10, 12, :punct),
-                   Tokenizer::Token.new(12, 13, :punct),
-                   Tokenizer::Token.new(13, 15, :break),
-                   Tokenizer::Token.new(15, 21, :letter),
-                   Tokenizer::Token.new(21, 22, :punct)])
+          Set.new([Entity.new(0,  10, :letter),
+                   Entity.new(10, 12, :punct),
+                   Entity.new(12, 13, :punct),
+                   Entity.new(13, 15, :break),
+                   Entity.new(15, 21, :letter),
+                   Entity.new(21, 22, :punct)])
         )
       end
 
       it 'can handle separated integers' do
         Tokenizer.new('228/359').tokens.must_equal(
-          Set.new([Tokenizer::Token.new(0, 3, :integer),
-                   Tokenizer::Token.new(3, 4, :separ),
-                   Tokenizer::Token.new(4, 7, :integer)])
+          Set.new([Entity.new(0, 3, :integer),
+                   Entity.new(3, 4, :separ),
+                   Entity.new(4, 7, :integer)])
         )
       end
     end
