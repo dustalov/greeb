614fc4f0d4ab705000e52ff23e8dbbe9cf7cb3c7
Dmitry Ustalov <dmitry@eveel.ru>
2013-08-19 01:44:43 +0600

    Move bin/greeb functionality into Greeb::Core

---
+++ lib/greeb/core.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+# Greeb::Core is a simple tool that allows to invoke Greeb::Tokenizer and
+# Greeb::Parser facilities together in a convinient and coherent way.
+#
+module Greeb::Core
+  # Greeb::Core uses several helpers from Greeb::Parser to perform
+  # additional analysis using there heuristic methods.
+  #
+  HELPERS = [:urls, :emails, :abbrevs]
+
+  # Recognize e-mail addresses in the input text.
+  #
+  # @param text [String] input text.
+  #
+  # @return [Array<Greeb::Entity>] a set of tokens.
+  #
+  def analyze text
+    Greeb::Tokenizer.tokenize(text).tap do |tokens|
+      HELPERS.each do |helper|
+        Greeb::Parser.public_send(helper, text).each do |parsed|
+          extract_tokens(tokens, parsed)
+        end
+      end
+    end
+  end
+
+  alias_method :'[]', :analyze
+
+  protected
+  # Extact tokens of the specified type from the input tokens set.
+  #
+  # @param tokens [Array<Greeb::Entity>] input tokens set.
+  # @param entity [Greeb::Entity] token to be extracted.
+  #
+  # @return [Greeb::Entity] token to be extracted.
+  #
+  def extract_tokens(tokens, entity)
+    from = tokens.index { |e| e.from == entity.from }
+    to = tokens.index { |e| e.to == entity.to }
+    tokens[from..to] = entity
+  end
+end
+
+Greeb.send(:extend, Greeb::Core)
---
+++ spec/core_spec.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+require_relative 'spec_helper'
+
+module Greeb
+  describe Greeb do
+    it 'should do nothing when ran without input' do
+      Greeb[''].must_be_empty
+    end
+
+    it 'should tokenize text when input is given' do
+      Greeb['Hello guys!'].must_equal(
+        [Entity.new(0, 5, :letter),
+         Entity.new(5, 6, :separ),
+         Entity.new(6, 10, :letter),
+         Entity.new(10, 11, :punct)]
+      )
+    end
+
+    it 'should extract URLs' do
+      Greeb['Hello http://nlpub.ru guys!'].must_equal(
+        [Entity.new(0, 5, :letter),
+         Entity.new(5, 6, :separ),
+         Entity.new(6, 21, :url),
+         Entity.new(21, 22, :separ),
+         Entity.new(22, 26, :letter),
+         Entity.new(26, 27, :punct)]
+      )
+    end
+
+    it 'should extract e-mails' do
+      Greeb['Hello example@example.com guys!'].must_equal(
+        [Entity.new(0, 5, :letter),
+         Entity.new(5, 6, :separ),
+         Entity.new(6, 25, :email),
+         Entity.new(25, 26, :separ),
+         Entity.new(26, 30, :letter),
+         Entity.new(30, 31, :punct)]
+      )
+    end
+  end
+end
